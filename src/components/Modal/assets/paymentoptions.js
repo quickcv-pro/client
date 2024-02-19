c51609f0ae34a8aaf4ac6c0c17e20af7e062cd89
Oluniyisrael <israeloluniyi2004@gmail.com>
2024-02-19 13:35:19 +0100

    I pause here

--- src/components/Modal/assets/paymentoptions.js
+++ src/components/Modal/assets/paymentoptions.js
@@ -2,6 +2,17 @@ const paymentOptions = [
     {
         Name: "Paystack",
         Logo: "https//:blah.com",
-        
+    },
+    {
+        Name: "Stripe",
+        Logo: "https//:blah.com",
+    },
+    {
+        Name: "PayPal",
+        Logo: "https//:blah.com",
+    },
+    {
+        Name: "Budpay",
+        Logo: "https//:blah.com",
     }
 ]
